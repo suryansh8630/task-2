3236f0ed0bf489072dc99bcc0f6c55c5834af66e
suryansh8630 <139366776+suryansh8630@users.noreply.github.com>
2024-04-01 00:57:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
 import unittest
-from flask import Flask, redirect, url_for
+from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def hello():
     try:
         response = requests.get("https://atg.world")
         if response.status_code == 200:
-            return redirect(url_for('hello'))
+            return "Website loaded successfully!", 200
         else:
             return "Failed to load website.", 500
     except requests.ConnectionError:
         return "Failed to connect to website.", 500
-
 @app.route('/hello')
-def hello():
-    return 'Hello, World!'
+def ind():
+    return 'Hello, World!'       
 
 class TestWebsiteConnection(unittest.TestCase):
     def setUp(self):
@@ -27,9 +26,11 @@ class TestWebsiteConnection(unittest.TestCase):
     def test_website_loading(self):
         print("Testing connection to atg.world website...")
         response = self.app.get('/')
-        self.assertEqual(response.status_code, 302)  # Expecting a redirect
-        self.assertEqual(response.location, 'http://localhost/hello')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "Website loaded successfully!")
+        self.assertEqual(response.location, 'http://20.197.8.46/hello')
+        print("atg.world website loaded successfully!")
         print("Website redirected to Hello World Flask route successfully!")
-
+        
 if __name__ == "__main__":
     unittest.main()
