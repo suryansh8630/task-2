17364e4e29e5f3c09d350352b7eda23766799605
suryansh8630 <139366776+suryansh8630@users.noreply.github.com>
2024-03-31 21:20:17 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+import requests
+
+class TestWebsiteConnection(unittest.TestCase):
+    
+    def test_website_connection(self):
+        url = 'https://atg.world'
+        
+        print("Attempting to connect to", url)
+        try:
+            response = requests.get(url)
+            status_code = response.status_code
+            print("Status code:", status_code)
+            
+            self.assertEqual(status_code, 200)  # Assuming 200 is the expected status code for successful connection
+            print("Website loaded successfully!")
+        except Exception as e:
+            print("Failed to connect to website:", e)
+            self.fail("Failed to connect to website")
+
+if __name__ == '__main__':
+    unittest.main()
